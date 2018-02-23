b9aa13369c0bef50dde2879f89518087e7a375e8
Claus Wiingreen <cwiing13@student.aau.dk>
2018-02-23 10:41:47 +0100

    Added references to vulkan and tested the connection.

--- papago-api-core/src/main.cpp
+++ papago-api-core/src/main.cpp
@@ -3,6 +3,10 @@
 int main()
 {
 	std::cout << "Hello, world!" << std::endl;
+	auto instance = vk::createInstance(vk::InstanceCreateInfo());
+	std::cout << "Created a vulkan instance." << std::endl;
 	char ch;
 	std::cin >> ch;
+
+	instance.destroy();
 }
