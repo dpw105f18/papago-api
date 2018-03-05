6f1c583f7c8c8dee6c73f8fd7fdbec76541ec079
Claus Wiingreen <cwiing13@student.aau.dk>
2018-03-05 11:35:32 +0100

    Small changes and fixes to the examples, lots of typos were fixed :D

--- Specification/Examples2/ShadowMap.cpp
+++ Specification/Examples2/ShadowMap.cpp
@@ -8,57 +8,57 @@ class object(){
 }
 
 std::vector<Object> objects = // Initializer list of objects to draw
-auto surface = // Get surface reference
+Surface surface = // Get surface reference
 
-VertexShaderByteCode shadowVertexShader = VertexShader::CompileFromFile("shadow.vert"); 
-FragmentShaderByteCode shadowFragmentShader = FragmentShader::CompileFromFile("shadow.frag");
-auto shadowPass = RenderPass(shadowVertexShader, shadowFragmentShader);
+VertexShader shadowVertexShader = VertexShader::CompileFromFile("shadow.vert"); 
+FragmentShader shadowFragmentShader = FragmentShader::CompileFromFile("shadow.frag");
+RenderPass shadowPass = RenderPass(shadowVertexShader, shadowFragmentShader);
 
-VertexShaderByteCode vertexShader = VertexShader::CompileFromFile("main.vert"); 
-FragmentShaderByteCode fragmentShader = FragmentShader::CompileFromFile("main.frag"); 
-auto mainPass = RenderPass(vertexShader, fragmentShader); 
+VertexShader vertexShader = VertexShader::CompileFromFile("main.vert"); 
+FragmentShader fragmentShader = FragmentShader::CompileFromFile("main.frag"); 
+RenderPass mainPass = RenderPass(vertexShader, fragmentShader); 
 
 // Creates 3 colorbuffers in a tripple buffer swapchain
-Swapchain swapChain = SwapChain(1024, 1024, RGBA, 3, FIFO_BUFFERING, surface);
-Image depthBuffer = Image(1024, 1024, RGBA ,IMAGE_DEPTH_BUFFER);
-Image shadowDepthBuffer = Image(1024, 1024, RGBA ,IMAGE_DEPTH_BUFFER);
+SwapChain swapChain = SwapChain(1024, 1024, RGBA, 3, FIFO_BUFFERING, surface);
+Image depthBuffer = Image(1024, 1024, S32Float, IMAGE_DEPTH_BUFFER);
+Image shadowDepthBuffer = Image(1024, 1024, S32Float, IMAGE_DEPTH_BUFFER);
 
-Sampler sampler2d();
+Sampler2D sampler = Sampler2D();
 sampler.SetMagFilter(NEAREST);
 sampler.SetMinFilter(NEAREST);
 sampler.SetTextureWrapS(CLAMP_TO_EDGE);
 sampler.SetTextureWrapT(CLAMP_TO_EDGE);
 
-auto framebuffers = new std::Vector<Framebuffer>();
-for(auto i =0; i < 3 ; ++i){
+std::vector<Framebuffer> framebuffers = std::vector<Framebuffer>();
+for(int i = 0; i < 3 ; ++i){
 	framebuffers.push_back({swapChain[i], depthBuffer});
-}	
+}
 
 while(true){
-	auto index = swapChain.CurrentIndex();
+	int index = swapChain.CurrentIndex();
 	
-	auto cbuf1 = PrimaryCommandBuffer(shadowPass);
-	cbuf1.AddPrimitiveTopology(TRIANGLES);
-	cBuf1.AddDepthBuffer(shadowDepthBuffer); // How do we set this when we do not have a color buffer?
-	cbuf1.AddDepthBufferClear();
+	CommandBuffer cBuf1 = CommandBuffer(shadowPass);
+	cBuf1.setPrimitiveTopology(TRIANGLES);
+	cBuf1.setDepthBuffer(shadowDepthBuffer); // How do we set this when we do not have a color buffer?
+	cBuf1.clearDepthBuffer();
+    cBuf1.setDepthTest(PPG_LESS);
 
-	auto sBuf1 = SubCommandBuffer(); 
-	for(auto& obj : objects){
-		sBuf1.AddUniform("depthMVP", obj.depthMVP);
-		sBuf1.AddInput("position", obj.position);
-		sBuf1.AddIndexBuffer(obj.indices); 
-		sBuf1.AddDrawInstanced(obj.indices.size(), 1, 0, 0);
+	SubCommandBuffer sBuf1 = SubCommandBuffer(); 
+	for(Object& obj : objects){
+		sBuf1.setUniform("depthMVP", obj.depthMVP);
+		sBuf1.setInput("position", obj.position);
+		sBuf1.setIndexBuffer(obj.indices); 
+		sBuf1.drawInstanced(obj.indices.size(), 1, 0, 0);
 	}
-	cbuf1.attatchBuffers({sBuf1});
-	shadowPass.setCommands({cbuf1});
+	cBuf1.attatchBuffers({sBuf1});
+	shadowPass.setCommands({cBuf1});
 
-	cbuf2 = PrimaryCommandBuffer(mainPass);
-	cbuf2.AddPrimitiveTopology(TRIANGLES);
-	cBuf1.AddOutput("outColor", framebuffers[index]);
-	cbuf2.AddFrameBufferClear(0.0f, 0.0f, 0.0f, 1.0f);
-	cbuf2.AddDepthBufferClear(1.0f);
-	cBuf2.AddDepthTest(PPG_LESS);
-
+	cBuf2 = PrimaryCommandBuffer(mainPass);
+	cBuf2.setPrimitiveTopology(TRIANGLES);
+	cBuf2.setOutput("outColor", framebuffers[index]);
+	cBuf2.clearFrameBuffer(0.0f, 0.0f, 0.0f, 1.0f);
+	cBuf2.clearDepthBuffer(1.0f);
+	cBuf2.setDepthTest(PPG_LESS);
 
 	cBuf2.setUniform("shadowMap", shadowDepthBuffer, sampler);
 	auto sBuf2 = SubCommandBuffer(); 
@@ -72,5 +72,6 @@ while(true){
 	}
 	cbuf2.attatchBuffers({sBuf2});
 	mainPass.setCommands({cBuf2});
+    swapChain.submitPass(mainPass);
 	swapChain.present();
 }
