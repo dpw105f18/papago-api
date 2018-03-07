1dbeccf0ebc3600d4e3bdcaed878801b4720cf7d
Claus Wiingreen <cwiing13@student.aau.dk>
2018-03-07 15:12:33 +0100

    More improvements to the specification

--- Specification/Examples2/ShadowMap.cpp
+++ Specification/Examples2/ShadowMap.cpp
@@ -2,13 +2,16 @@ class object(){
 	mat4 depthMVP;
 	mat4 MVP;
 	mat4 depthBiasMVP;
-	vec3 positions[];
-	vec3 color[];
-	vec3 indices[];
+    vector<vec3> positions;
+    vector<vec3> colors;
+	vector<int> indices;
 }
 
-std::vector<Object> objects = // Initializer list of objects to draw
-Surface surface = // Get surface reference
+std::vector<Object> objects = {
+    // Initializer list of objects to draw
+};
+Surface surface = Surface( /* Get surface reference */ );
+GraphicsQueue graphics_queue = surface.get_graphics_queue();
 
 VertexShader shadowVertexShader = VertexShader::CompileFromFile("shadow.vert"); 
 FragmentShader shadowFragmentShader = FragmentShader::CompileFromFile("shadow.frag");
@@ -19,8 +22,8 @@ FragmentShader fragmentShader = FragmentShader::CompileFromFile("main.frag");
 RenderPass mainPass = RenderPass(vertexShader, fragmentShader); 
 
 // Creates 3 colorbuffers in a tripple buffer swapchain
-SwapChain swapChain = SwapChain(1024, 1024, RGBA, 3, FIFO_BUFFERING, surface);
-Image depthBuffer = Image(1024, 1024, S32Float, IMAGE_DEPTH_BUFFER);
+SwapChain swapChain = SwapChain(RGBA, 3, FIFO_BUFFERING, surface);
+Image depthBuffer = Image(surface.width(), surface.height(), S32Float, IMAGE_DEPTH_BUFFER);
 Image shadowDepthBuffer = Image(1024, 1024, S32Float, IMAGE_DEPTH_BUFFER);
 
 Sampler2D sampler = Sampler2D();
@@ -29,49 +32,44 @@ sampler.SetMinFilter(NEAREST);
 sampler.SetTextureWrapS(CLAMP_TO_EDGE);
 sampler.SetTextureWrapT(CLAMP_TO_EDGE);
 
-std::vector<Framebuffer> framebuffers = std::vector<Framebuffer>();
-for(int i = 0; i < 3 ; ++i){
-	framebuffers.push_back({swapChain[i], depthBuffer});
+CommandBuffer cBuf1 = CommandBuffer(shadowPass);
+cBuf1.setPrimitiveTopology(TRIANGLES);
+cBuf1.setDepthBuffer(shadowDepthBuffer);
+cBuf1.clearDepthBuffer();
+cBuf1.setDepthTest(PPG_LESS);
+
+SubCommandBuffer sBuf1 = SubCommandBuffer(); 
+for(Object& obj : objects){
+    sBuf1.setUniform("depthMVP", obj.depthMVP);
+    sBuf1.setInput("position", obj.position);
+    sBuf1.setIndexBuffer(obj.indices); 
+    sBuf1.drawInstanced(obj.indices.size(), 1, 0, 0);
 }
+cBuf1.attatchBuffers({sBuf1});
+shadowPass.setCommands(cBuf1);
 
-while(true){
-	int index = swapChain.CurrentIndex();
-	
-	CommandBuffer cBuf1 = CommandBuffer(shadowPass);
-	cBuf1.setPrimitiveTopology(TRIANGLES);
-	cBuf1.setDepthBuffer(shadowDepthBuffer); // How do we set this when we do not have a color buffer?
-	cBuf1.clearDepthBuffer();
-    cBuf1.setDepthTest(PPG_LESS);
+cBuf2 = PrimaryCommandBuffer(mainPass);
+cBuf2.setPrimitiveTopology(TRIANGLES);
+cBuf2.setOutput("outColor", swapchain);
+cBuf2.setDepthBuffer(depthBuffer);
+cBuf2.clearFrameBuffer(0.0f, 0.0f, 0.0f, 1.0f);
+cBuf2.clearDepthBuffer(1.0f);
+cBuf2.setDepthTest(PPG_LESS);
 
-	SubCommandBuffer sBuf1 = SubCommandBuffer(); 
-	for(Object& obj : objects){
-		sBuf1.setUniform("depthMVP", obj.depthMVP);
-		sBuf1.setInput("position", obj.position);
-		sBuf1.setIndexBuffer(obj.indices); 
-		sBuf1.drawInstanced(obj.indices.size(), 1, 0, 0);
-	}
-	cBuf1.attatchBuffers({sBuf1});
-	shadowPass.setCommands({cBuf1});
+cBuf2.setUniform("shadowMap", shadowDepthBuffer, sampler);
+auto sBuf2 = SubCommandBuffer(); 
+for(auto& obj : objects){	
+    sBuf2.setUniform("MVP", obj.MVP);
+    sBuf2.setUniform("depthBiasMVP", obj.depthBiasMVP);
+    sBuf2.setInput("position", obj.positions);
+    sBuf2.setInput("color", obj.colors);
+    sBuf2.setIndexBuffer(obj.indices);
+    sBuf2.drawInstanced(obj.indices.size(), 1, 0, 0);
+}
+cbuf2.attatchBuffers({sBuf2});
+mainPass.setCommands(cBuf2);
 
-	cBuf2 = PrimaryCommandBuffer(mainPass);
-	cBuf2.setPrimitiveTopology(TRIANGLES);
-	cBuf2.setOutput("outColor", framebuffers[index]);
-	cBuf2.clearFrameBuffer(0.0f, 0.0f, 0.0f, 1.0f);
-	cBuf2.clearDepthBuffer(1.0f);
-	cBuf2.setDepthTest(PPG_LESS);
-
-	cBuf2.setUniform("shadowMap", shadowDepthBuffer, sampler);
-	auto sBuf2 = SubCommandBuffer(); 
-	for(auto& obj : objects){	
-		sBuf2.AddUniform("MVP", obj.MVP);
-		sBuf2.AddUniform("depthBiasMVP", obj.depthBiasMVP);
-		sBuf2.AddInput("position", obj.position);
-		sBuf2.AddInput("color", obj.color);
-		sBuf2.AddIndexBuffer(obj.indices);
-		sBuf2.AddDrawInstanced(obj.indices.size(), 1, 0, 0);
-	}
-	cbuf2.attatchBuffers({sBuf2});
-	mainPass.setCommands({cBuf2});
-    swapChain.submitPass(mainPass);
+while(true){	
+    graphics_queue.submitPass(mainPass);
 	swapChain.present();
 }
