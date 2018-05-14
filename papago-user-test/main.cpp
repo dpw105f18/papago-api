fd4ed566c4c9549123d497f234a31d50d985d35c
Anders M <AndersM91@hotmail.dk>
2018-05-14 07:00:02 +0200

    - added glm - added folders for textures and shaders - added readFile/(...) and readPixels(...) function to util.h

--- papago-user-test/main.cpp
+++ papago-user-test/main.cpp
@@ -1,10 +1,75 @@
+//FPS counting and displaying in window:
+#include <chrono>
+#include <sstream>
+
+//GLM - for glsl types in user code:
+#define GLM_ENABLE_EXPERIMENTAL
+#include "external/glm/glm.hpp"
+#include "external/glm/gtx/transform.hpp"
+
+//File read functions (readFile(...) and readPixels(...))
 #include "util.h"
+
+//API::
 #include "papago.hpp"
+
+//convenience:
+#define PARSER_COMPILER_PATH "C:/VulkanSDK/1.0.65.0/Bin/glslangValidator.exe"
+using CommandBufferRefCollection = std::vector<std::reference_wrapper<ICommandBuffer>>;
+
+struct Vertex
+{
+	glm::vec3 pos;
+	glm::vec2 uv;
+};
+
 
 int main()
 {
-	auto hwnd = StartWindow(800, 600);
-	auto surface = ISurface::createWin32Surface(800, 600, hwnd);
+	
+	//init
+	auto windowWidth = 800;
+	auto windowHeight = 600;
+	auto hwnd = StartWindow(windowWidth, windowHeight);
 
-	auto& device = IDevice::enumerateDevices(*surface, {}, {})[0];
+	//Main game loop:
+	using Clock = std::chrono::high_resolution_clock;
+	auto lastUpdate = Clock::now();
+	auto lastFrame = Clock::now();
+	long fps = 0;
+	bool run = true;
+
+	while (run)
+	{
+		MSG msg;
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT) {
+				run = false;
+			}
+			
+			//Handle input:
+
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else {
+			//FPS counter:
+			auto deltaTime = (Clock::now() - lastUpdate);
+			auto frameTime = (Clock::now() - lastFrame);
+			lastFrame = Clock::now();
+
+			using namespace std::chrono_literals;
+			if (deltaTime > 1s) {
+				lastUpdate = Clock::now();
+				std::stringstream ss;
+				ss << "FPS: " << fps
+					<< " --- Avg. Frame Time: " << 1000.0 / fps << "ms"
+					<< " --- Last Frame Time: " << std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(frameTime).count() << "ms";
+				SetWindowName(hwnd, ss.str());
+				fps = 0;
+			}
+
+			//Handle game logic:
+		}
 }
--- papago-user-test/util.h
+++ papago-user-test/util.h
@@ -3,6 +3,11 @@
 #include <Windows.h>
 #include <WinUser.h>
 #include <iostream>
+#include <fstream>
+
+//STB - for image loading
+#define STB_IMAGE_IMPLEMENTATION
+#include "external/stb/stb_image.h"
 
 LRESULT CALLBACK wndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
@@ -88,3 +93,27 @@ void SetWindowName(HWND hwnd, const std::string& text)
 	SetWindowText(hwnd, text.c_str());
 }
 
+std::string readFile(const std::string& file_path) {
+	std::ifstream stream(file_path, std::ios::ate);
+	auto size = stream.tellg();
+	stream.seekg(0);
+	std::string result = std::string(size, '\0');
+	stream.read(&result[0], size);
+	return result;
+}
+
+std::vector<char> readPixels()
+{
+	int texWidth, texHeight, texChannels;
+	auto pixels = stbi_load("textures/eldorado.jpg", &texWidth, &texHeight, &texChannels, STBI_rgb_alpha);
+	if (!pixels) {
+		throw new std::runtime_error("Failed to load texture image!");
+	}
+
+	auto result = std::vector<char>();
+	result.resize(texWidth * texHeight * 4);
+	memcpy(result.data(), pixels, result.size());
+	stbi_image_free(pixels);
+
+	return result;
+}
